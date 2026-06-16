711e2cca6f9533cc7943f5a64c50d9e691c6481b
Stein <miguel.wainst@gmail.com>
2026-06-15 22:06:09 -0300

    Continuando os testes

--- 3 - Library API/libraryapi/src/test/java/com/dev_curso/libraryapi/repository/AutorRepositoryTest.java
+++ 3 - Library API/libraryapi/src/test/java/com/dev_curso/libraryapi/repository/AutorRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -15,17 +16,19 @@ public class AutorRepositoryTest {
     @Autowired
     AutorRepository repository;
 
+    // Salva um autor no bd
     @Test
     public void salvarTest() {
         Autor autor = new Autor();
-        autor.setNome("Emilly");
+        autor.setNome("RandomGuy");
         autor.setNacionalidade("Brasileira");
-        autor.setDataNascimento(LocalDate.of(2007, 5, 12));
+        autor.setDataNascimento(LocalDate.of(2003, 7, 13));
 
         var autorSalvo = repository.save(autor);
         System.out.println("Autor salvo: " + autorSalvo);
     }
 
+    // Atualiza um autor
     @Test
     public void atualizarTest() {
         var id = UUID.fromString("6c1943be-3e1c-4ccd-a881-18ac51cf392e");
@@ -43,5 +46,33 @@ public class AutorRepositoryTest {
             repository.save(autorEncontrado);
         }
 
+    }
+
+    // Lista todos os autores
+    @Test
+    public void listarTest() {
+        List<Autor> list = repository.findAll();
+        list.forEach(System.out::println);
+    }
+
+    // Conta quantos autores tem
+    @Test
+    public void countTest() {
+        System.out.println("Contagem de autores: " + repository.count());
+    }
+
+    // Deleta um autor por id
+    @Test
+    public void deletePorIdTeste() {
+        var id = UUID.fromString("539f1903-770e-4b05-b161-22ad10ea587e");
+        repository.deleteById(id);
+    }
+
+    // Delata por objeto
+    @Test
+    public void deletePorObjectTeste() {
+        var id = UUID.fromString("539f1903-770e-4b05-b161-22ad10ea587e");
+        var maria = repository.findById(id).get();
+        repository.delete(maria);
     }
 }
