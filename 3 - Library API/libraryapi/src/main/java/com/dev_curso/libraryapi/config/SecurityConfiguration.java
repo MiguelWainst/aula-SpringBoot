2c1af0d6892917a70308b6eb2325d734181c052d
Miguel Wainstein <miguel.wainst@gmail.com>
2026-07-20 20:53:30 -0300

    add: UsuarioDetailService

--- 3 - Library API/libraryapi/src/main/java/com/dev_curso/libraryapi/config/SecurityConfiguration.java
+++ 3 - Library API/libraryapi/src/main/java/com/dev_curso/libraryapi/config/SecurityConfiguration.java
@@ -1,22 +1,20 @@
 package com.dev_curso.libraryapi.config;
 
-import org.jspecify.annotations.Nullable;
+import com.dev_curso.libraryapi.security.CustomUserDetailService;
+import com.dev_curso.libraryapi.service.UsuarioService;
 import org.springframework.beans.factory.aspectj.ConfigurableObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import static org.springframework.security.config.Customizer.*;
-
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -61,20 +59,22 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        UserDetails user1 = User.builder()
-                .username("user")
-                .password(encoder.encode("123"))
-                .roles("USER")
-                .build();
+    public UserDetailsService userDetailsService(UsuarioService usuarioService) {
+//        UserDetails user1 = User.builder()
+//                .username("user")
+//                .password(encoder.encode("123"))
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails user2 = User.builder()
+//                .username("admin")
+//                .password(encoder.encode("321"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user1, user2);
 
-        UserDetails user2 = User.builder()
-                .username("admin")
-                .password(encoder.encode("321"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user1, user2);
+        return new CustomUserDetailService(usuarioService);
     }
 
 }
--- 3 - Library API/libraryapi/src/main/java/com/dev_curso/libraryapi/repository/AutorRepository.java
+++ 3 - Library API/libraryapi/src/main/java/com/dev_curso/libraryapi/repository/AutorRepository.java
@@ -4,7 +4,6 @@ import com.dev_curso.libraryapi.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import javax.swing.text.html.Option;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
