9dec68f12e233c956d6ec72a9a5334eb029927b0
Miguel Wainstein <miguel.wainst@gmail.com>
2026-07-27 12:13:05 -0300

    fix: consertando os imports

--- 3 - Library API/libraryapi/src/main/java/com/dev_curso/libraryapi/config/SecurityConfiguration.java
+++ 3 - Library API/libraryapi/src/main/java/com/dev_curso/libraryapi/config/SecurityConfiguration.java
@@ -2,17 +2,14 @@ package com.dev_curso.libraryapi.config;
 
 import com.dev_curso.libraryapi.security.CustomUserDetailService;
 import com.dev_curso.libraryapi.service.UsuarioService;
-import org.springframework.beans.factory.aspectj.ConfigurableObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
